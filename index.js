ee81b02b28821db21ae0ee83b75564fd8c817196
Ethan Swafford <ethanswafford@yahoo.com>
2022-05-23 19:48:11 -0400

    secPush

--- index.js
+++ index.js
@@ -8,4 +8,13 @@ const app = express()
 app.get('/', (req, res) => {
     res.json('This is a climate monitoring API service')
 })
+
+app.get('/news', (req, res) => {
+    axios.get('https://www.climate.gov/news-features/understanding-climate/climate-change-global-temperature')
+        .then((response) => {
+            const html = response.data
+            res.json('This the current events page')
+            console.log(html)
+        })
+})
 app.listen(PORT, () => console.log(`server initialized on PORT: ${PORT}`))
