c16d6305103fe4a599f759cee70e1b0b45e21c06
Ethan Swafford <ethanswafford@yahoo.com>
2022-05-24 01:44:09 -0400

    secPush

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const axios = require("axios")
 const cheerio = require("cheerio")
 
 const app = express()
+const articles = []
 
 app.get('/', (req, res) => {
     res.json('This is a climate monitoring API service')
@@ -12,9 +13,20 @@ app.get('/', (req, res) => {
 app.get('/news', (req, res) => {
     axios.get('https://www.climate.gov/news-features/understanding-climate/climate-change-global-temperature')
         .then((response) => {
+
             const html = response.data
-            res.json('This the current events page')
-            console.log(html)
-        })
+            const $ = cheerio.load(html)
+
+            $('a:contains("climate")', html).each(function() {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+
+                articles.push({
+                    title,
+                    url
+                })
+            })
+            res.json(articles)
+        }).catch((err) => console.log(err))
 })
 app.listen(PORT, () => console.log(`server initialized on PORT: ${PORT}`))
