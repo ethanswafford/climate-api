71b8cf12fb3d134d6c8b089b0ca0620c358a39b7
Ethan Swafford <ethanswafford@yahoo.com>
2022-05-24 02:01:24 -0400

    secPush

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const PORT = 8000
+// global variable and array declarations and
+// required node modules 
+
+const PORT = 8888
 const express = require("express")
 const axios = require("axios")
 const cheerio = require("cheerio")
@@ -6,9 +9,15 @@ const cheerio = require("cheerio")
 const app = express()
 const articles = []
 
+// root dir and starting page 
+
 app.get('/', (req, res) => {
     res.json('This is a climate monitoring API service')
 })
+
+// scrape website or url for any 'href' containing the word "climate"
+// stored response.data in local variable 'html' and use local variables 'url' and 'title' to 
+// order a function to organize and render content
 
 app.get('/news', (req, res) => {
     axios.get('https://www.climate.gov/news-features/understanding-climate/climate-change-global-temperature')
@@ -29,4 +38,7 @@ app.get('/news', (req, res) => {
             res.json(articles)
         }).catch((err) => console.log(err))
 })
+
+// built in node module function using 'start' under test in the package.json file 
+
 app.listen(PORT, () => console.log(`server initialized on PORT: ${PORT}`))
